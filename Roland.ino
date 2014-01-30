bd2402d27c4956b76732605140a76ce21875d1b6
Mikael Jorhult <mikael@jorhult.se>
2014-01-30 14:04:17 +0100

    Arduino arrays are shit...

--- Roland.ino
+++ Roland.ino
@@ -1,10 +1,10 @@
 /***************************************
-  *                                    *
-  *                                    *
-  * Roland, the Headless MIDI Drummer  *
-  *                                    *
-  *                                    *
-  **************************************/
+ *                                    *
+ *                                    *
+ * Roland, the Headless MIDI Drummer  *
+ *                                    *
+ *                                    *
+ **************************************/
 
 // Include the motor shield
 #include <Wire.h>
@@ -12,23 +12,19 @@
 #include "utility/Adafruit_PWMServoDriver.h"
 
 byte commandByte = 0;
-int drumMotors[ 4 ];
 
 // Motor
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-
-
+Adafruit_DCMotor *kickMotor = AFMS.getMotor( 1 );
+Adafruit_DCMotor *snareMotor = AFMS.getMotor( 2 );
+Adafruit_DCMotor *hihatMotor = AFMS.getMotor( 3 );
+Adafruit_DCMotor *crashMotor = AFMS.getMotor( 4 );
+Adafruit_DCMotor *currentMotor;
 
 void setup() {
-  // Initialize addresses to drums.
-  drumMotors[ 0x24 ] = 1;
-  drumMotors[ 0x26 ] = 2;
-  drumMotors[ 0x2A ] = 3;
-  drumMotors[ 0x31 ] = 4;
-  
   // Setup serial interface for MIDI.
   Serial.begin( 31250 );
-  
+
   // Initialize motor shield.
   AFMS.begin();
 }
@@ -42,45 +38,65 @@ void checkMIDI() {
   if ( Serial.available() ) {
     // Read first byte (command).
     commandByte = Serial.read();
-    
+
     // Just listen for note on and off.
     if ( commandByte == 0x90 || commandByte == 0x80 ) {
       byte values[ 2 ];
-      int drumMotor = 0;
       getBytes( 2, values );
-      drumMotor = getMotor( values[ 0 ] );
       
-      if ( drumMotor > 0 ) {
-        drum( drumMotor, values[ 1 ] );
+      if ( getMotor( values[ 0 ] ) ) {
+        drum( values[ 1 ] );
       }
     }
   }
 }
 
-void drum( int myDrum, byte velocity ) {
-  Adafruit_DCMotor *drumMotor = AFMS.getMotor( myDrum );
-  
+void drum( byte velocity ) {
   if ( velocity > 0 ) { // HIT
     int motorSpeed = ( byte ) velocity + 128; // 255;
-    
-    drumMotor->setSpeed( motorSpeed );
-    drumMotor->run( FORWARD );
-  } else { // RELEASE
-    drumMotor->run( RELEASE );
+
+    currentMotor->setSpeed( motorSpeed );
+    currentMotor->run( FORWARD );
+  } 
+  else { // RELEASE
+    currentMotor->run( RELEASE );
   }
 }
 
-int getMotor( byte nB ) {
-  return drumMotors[ nB ];
+boolean getMotor( byte nB ) {
+  switch ( nB ) {
+    case 0x24: // C1, Kick
+      currentMotor = kickMotor;
+      break;    
+      
+    case 0x26: // D1, Snare
+      currentMotor = snareMotor;
+      break;
+    
+    case 0x2A: // F#1, HiHat
+      currentMotor = hihatMotor;
+      break;
+      
+    case 0x31: // Crash
+      currentMotor = crashMotor;
+      break;
+    
+    default: // Other
+      return false;
+  }
+  
+  return true;
 }
 
 void getBytes( int expectedBytes, byte savedBytes[] ) {
   int receivedBytes = 0;
-  
+
   do {
     if ( Serial.available() ) {
       savedBytes[ receivedBytes ] = Serial.read();
       receivedBytes++;
     }
-  } while( receivedBytes < expectedBytes );
+  } 
+  while( receivedBytes < expectedBytes );
 }
+
